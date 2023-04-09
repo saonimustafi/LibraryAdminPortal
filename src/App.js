22770bc3e7c43382b4018e9a1ca9665308b36d99
saonimustafi <saonimustafi1994@gmail.com>
2023-04-09 02:32:01 -0700

    Add userActivitiesAdmin and checkUserFineAdmin pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import BookDetailPage from './pages/BookDetailPage';
 import SearchPage from './pages/SearchPage';
 import RegisterUserPage from './pages/RegisterUserPage';
 import SignInPage from './pages/SignInPage';
+import UserActivitiesPageAdmin from './pages/UserActivitiesPageAdmin';
+import CheckUserFinePageAdmin from './pages/CheckUserFinePageAdmin';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Route path="/register" element={<RegisterUserPage />} />
           <Route path="/search" element={<SearchPage />} />
           <Route path="/books/:id" element={<BookDetailPage />} />
+          <Route path="/userActivitiesAdmin" element={<UserActivitiesPageAdmin />} />
+          <Route path="/checkUserFineAdmin" element={<CheckUserFinePageAdmin />} />
         </Routes>
       </div>
     </Router>
